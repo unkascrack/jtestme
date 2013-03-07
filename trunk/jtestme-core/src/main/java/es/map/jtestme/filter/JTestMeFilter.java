93a20ae38ee81eaa60bbc62383d820514c6ea6c9
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@fd7ca0d8-11da-9f73-657e-4996fc3685d9>
2013-03-07 18:57:37 +0000

    Revisado el javax.servlet.Filter para mostrar los resources cuando recibe petición

--- trunk/jtestme-core/src/main/java/es/map/jtestme/filter/JTestMeFilter.java
+++ trunk/jtestme-core/src/main/java/es/map/jtestme/filter/JTestMeFilter.java
@@ -1,6 +1,9 @@
 package es.map.jtestme.filter;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.List;
 
@@ -86,7 +89,17 @@ public class JTestMeFilter implements Filter {
     public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
             throws IOException, ServletException {
         final long start = System.currentTimeMillis();
-        doMonitoring(request, response);
+
+        try {
+            if (request.getParameter(RESOURCE_PARAMETER) == null) {
+                doMonitoring(request, response);
+            } else {
+                doResource(request, response);
+            }
+        } catch (final Throwable e) {
+            JTestMeLogger.warn("JTestMeFilter fail doFilter: " + e.getMessage());
+        }
+
         final long duration = System.currentTimeMillis() - start;
         JTestMeLogger.info("JTestMe filter doFilter done in " + duration + " ms");
     }
@@ -94,9 +107,10 @@ public class JTestMeFilter implements Filter {
     /**
      * @param request
      * @param response
+     * @throws IOException
      */
-    private void doMonitoring(final ServletRequest request, final ServletResponse response) {
-        PrintWriter out = null;
+    private void doMonitoring(final ServletRequest request, final ServletResponse response) throws IOException {
+        PrintWriter output = null;
         try {
             final List<JTestMeResult> results = BUILDER.runExecutors();
             final JTestMeViewerType viewerType = getViewerType(request);
@@ -108,20 +122,54 @@ public class JTestMeFilter implements Filter {
                 ((HttpServletResponse) response).setHeader("Content-Disposition", "inline; filename=\"" + fileName
                         + "\"");
             }
-            out = response.getWriter();
-            out.println(viewer.getContentViewer(results));
-            out.flush();
-        } catch (final Throwable e) {
-            JTestMeLogger.warn("JTestMeFilter fail doMonitoring: " + e.getMessage());
+            output = response.getWriter();
+            output.println(viewer.getContentViewer(results));
+            output.flush();
         } finally {
-            if (out != null) {
-                out.close();
+            if (output != null) {
+                output.close();
+            }
+        }
+    }
+
+    private static final String RESOURCE_PARAMETER = "resource";
+
+    /**
+     * @param request
+     * @param response
+     * @throws IOException
+     */
+    private void doResource(final ServletRequest request, final ServletResponse response) throws IOException {
+        final String resource = request.getParameter(RESOURCE_PARAMETER);
+        final String localResource = "META-INF/resources/" + resource;
+        ((HttpServletResponse) response).addHeader("Cache-Control", "max-age=3600");
+        response.setContentType(config.getServletContext().getMimeType(resource));
+
+        final OutputStream output = response.getOutputStream();
+        final InputStream input = new BufferedInputStream(getClass().getResourceAsStream(localResource));
+        try {
+            final byte[] bytes = new byte[4 * 1024];
+            int length = input.read(bytes);
+            while (length != -1) {
+                output.write(bytes, 0, length);
+                length = input.read(bytes);
+            }
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+            if (output != null) {
+                output.close();
             }
         }
     }
 
     private static final String DEFAULT_FILENAME = "jtestme";
 
+    /**
+     * @param viewer
+     * @return
+     */
     private String getFileName(final JTestMeViewer viewer) {
         final String fileName = config.getInitParameter(PARAM_CONFIG_FILENAME);
         return (fileName != null && fileName.trim().length() > 0 ? fileName : DEFAULT_FILENAME) + viewer.getExtension();
@@ -129,6 +177,10 @@ public class JTestMeFilter implements Filter {
 
     private static final String DEFAULT_REQUEST_PARAM_FORMAT = "format";
 
+    /**
+     * @param request
+     * @return
+     */
     private JTestMeViewerType getViewerType(final ServletRequest request) {
         String requestParam = config.getInitParameter(PARAM_CONFIG_PARAMETER_FORMAT);
         requestParam = requestParam != null && requestParam.trim().length() > 0 ? requestParam
@@ -141,7 +193,7 @@ public class JTestMeFilter implements Filter {
     /**
      * @return
      */
-    public String getEncoding() {
+    private String getEncoding() {
         final String encoding = config.getInitParameter(PARAM_CONFIG_ENCODING);
         return encoding != null && encoding.trim().length() > 0 ? encoding : DEFAULT_ENCODING;
     }
