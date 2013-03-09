4d6aa1f6a879619136f02bceec1e1cd5d4796f27
carlos.alonso.gonzalez <carlos.alonso.gonzalez@gmail.com>
2013-03-09 00:41:57 +0000

    Añadido OpenOfficeExecutor y test

---
+++ jtestme-core/src/test/java/es/map/jtestme/executors/impl/OpenOfficeExecutorTest.java
@@ -0,0 +1,54 @@
+package es.map.jtestme.executors.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import es.map.jtestme.domain.JTestMeResult;
+
+public class OpenOfficeExecutorTest {
+
+    private OpenOfficeExecutor executor;
+
+    @Test
+    public void testExecutorTestMeParamsNull() {
+        executor = new OpenOfficeExecutor(null);
+        final JTestMeResult result = executor.executeTestMe();
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result.isSuscess());
+    }
+
+    @Test
+    public void testExecutorTestMeParamsEmpty() {
+        final Map<String, String> params = new HashMap<String, String>();
+        executor = new OpenOfficeExecutor(params);
+        final JTestMeResult result = executor.executeTestMe();
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result.isSuscess());
+    }
+
+    @Test
+    public void testExecutorTestMeParamsHostPortNotFound() {
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("host", "notfound");
+        params.put("port", "21");
+        executor = new OpenOfficeExecutor(params);
+        final JTestMeResult result = executor.executeTestMe();
+        Assert.assertNotNull(result);
+        Assert.assertFalse(result.isSuscess());
+    }
+
+    @Test
+    public void testExecutorTestMeParamsOk() {
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("host", "localhost");
+        params.put("port", "8700");
+        executor = new OpenOfficeExecutor(params);
+        final JTestMeResult result = executor.executeTestMe();
+        Assert.assertNotNull(result);
+        Assert.assertTrue(result.isSuscess());
+    }
+
+}
