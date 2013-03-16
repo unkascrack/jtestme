4d88f4f0420717952960e03b9a0fa8c20cd46a7f
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@fd7ca0d8-11da-9f73-657e-4996fc3685d9>
2013-03-16 11:06:06 +0000

    Revisada la factoría de creación de executors, para hacerla lo más independiente posible y que dependa del tipo de executor.

--- trunk/jtestme-core/src/main/java/es/jtestme/executors/JTestMeExecutorFactory.java
+++ trunk/jtestme-core/src/main/java/es/jtestme/executors/JTestMeExecutorFactory.java
@@ -2,18 +2,9 @@ package es.jtestme.executors;
 
 import java.util.Map;
 
-import es.jtestme.executors.impl.ConnectionExecutor;
-import es.jtestme.executors.impl.CustomExecutor;
-import es.jtestme.executors.impl.DatasourceExecutor;
-import es.jtestme.executors.impl.GraphicsExecutor;
-import es.jtestme.executors.impl.JDBCExecutor;
-import es.jtestme.executors.impl.JNDIExecutor;
 import es.jtestme.executors.impl.JTestMeDefaultExecutor;
-import es.jtestme.executors.impl.LDAPExecutor;
-import es.jtestme.executors.impl.OpenOfficeExecutor;
-import es.jtestme.executors.impl.SMTPExecutor;
-import es.jtestme.executors.impl.WebServiceExecutor;
 import es.jtestme.logger.JTestMeLogger;
+import es.jtestme.utils.JTestMeUtils;
 
 public final class JTestMeExecutorFactory {
 
@@ -39,38 +30,8 @@ public final class JTestMeExecutorFactory {
         JTestMeExecutor executor = null;
         if (executorType != null) {
             try {
-                switch (executorType) {
-                    case CONNECTION:
-                        executor = new ConnectionExecutor(params);
-                    break;
-                    case DATASOURCE:
-                        executor = new DatasourceExecutor(params);
-                    break;
-                    case JDBC:
-                        executor = new JDBCExecutor(params);
-                    break;
-                    case JNDI:
-                        executor = new JNDIExecutor(params);
-                    break;
-                    case GRAPHICS:
-                        executor = new GraphicsExecutor(params);
-                    break;
-                    case LDAP:
-                        executor = new LDAPExecutor(params);
-                    break;
-                    case OPENOFFICE:
-                        executor = new OpenOfficeExecutor(params);
-                    break;
-                    case SMTP:
-                        executor = new SMTPExecutor(params);
-                    break;
-                    case WEBSERVICE:
-                        executor = new WebServiceExecutor(params);
-                    break;
-                    case CUSTOM:
-                        executor = new CustomExecutor(params);
-                    break;
-                }
+                final String executorClassName = executorType.getExecutorClassName();
+                executor = JTestMeUtils.loadExecutor(executorClassName, name, params);
                 JTestMeLogger.info("JTestMe loading executor '" + name + "' of type: " + executorType);
             } catch (final Throwable e) {
                 JTestMeLogger.warn("JTestMe could not load executor '" + name + "' of type: " + executorType);
--- trunk/jtestme-core/src/main/java/es/jtestme/executors/JTestMeExecutorType.java
+++ trunk/jtestme-core/src/main/java/es/jtestme/executors/JTestMeExecutorType.java
@@ -1,17 +1,44 @@
 package es.jtestme.executors;
 
+import es.jtestme.executors.impl.ConnectionExecutor;
+import es.jtestme.executors.impl.CustomExecutor;
+import es.jtestme.executors.impl.DatasourceExecutor;
+import es.jtestme.executors.impl.FTPExecutor;
+import es.jtestme.executors.impl.GraphicsExecutor;
+import es.jtestme.executors.impl.JDBCExecutor;
+import es.jtestme.executors.impl.JNDIExecutor;
+import es.jtestme.executors.impl.LDAPExecutor;
+import es.jtestme.executors.impl.OpenOfficeExecutor;
+import es.jtestme.executors.impl.SMTPExecutor;
+import es.jtestme.executors.impl.WebServiceExecutor;
+
 public enum JTestMeExecutorType {
 
-    CONNECTION,
-    CUSTOM,
-    DATASOURCE,
-    GRAPHICS,
-    JDBC,
-    JNDI,
-    LDAP,
-    OPENOFFICE,
-    SMTP,
-    WEBSERVICE;
+    CONNECTION(ConnectionExecutor.class),
+    CUSTOM(CustomExecutor.class),
+    DATASOURCE(DatasourceExecutor.class),
+    FTP(FTPExecutor.class),
+    GRAPHICS(GraphicsExecutor.class),
+    JDBC(JDBCExecutor.class),
+    JNDI(JNDIExecutor.class),
+    LDAP(LDAPExecutor.class),
+    OPENOFFICE(OpenOfficeExecutor.class),
+    SMTP(SMTPExecutor.class),
+    WEBSERVICE(WebServiceExecutor.class);
+
+    private Class<? extends JTestMeExecutor> executorClass;
+
+    private JTestMeExecutorType(final Class<? extends JTestMeExecutor> executorClass) {
+        this.executorClass = executorClass;
+    }
+
+    public Class<? extends JTestMeExecutor> getExecutorClass() {
+        return executorClass;
+    }
+
+    public String getExecutorClassName() {
+        return executorClass.getName();
+    }
 
     public static JTestMeExecutorType toType(final String str) {
         JTestMeExecutorType type = null;
