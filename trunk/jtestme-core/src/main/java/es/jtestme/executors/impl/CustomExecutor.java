37d4355a1bbdd248190caca9e9b005b590a7be29
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@fd7ca0d8-11da-9f73-657e-4996fc3685d9>
2013-03-16 11:08:32 +0000

    Revisada la ejecución del custom executor para que sólo se instancie la clase predefinida por el usuario una única vez y no por cada petición de test que se realizaba.

--- trunk/jtestme-core/src/main/java/es/jtestme/executors/impl/CustomExecutor.java
+++ trunk/jtestme-core/src/main/java/es/jtestme/executors/impl/CustomExecutor.java
@@ -1,61 +1,49 @@
 package es.jtestme.executors.impl;
 
-import java.lang.reflect.Constructor;
 import java.util.Map;
 
 import es.jtestme.domain.JTestMeResult;
 import es.jtestme.executors.JTestMeExecutor;
+import es.jtestme.logger.JTestMeLogger;
+import es.jtestme.utils.JTestMeUtils;
 
 public class CustomExecutor extends JTestMeDefaultExecutor {
 
     private static final String PARAM_CLASS = "class";
 
-    private final String className;
+    private final JTestMeExecutor customExecutor;
 
     public CustomExecutor(final Map<String, String> params) {
         super(params);
-        className = getParamString(PARAM_CLASS);
+        final String className = getParamString(PARAM_CLASS);
+        removeParam(PARAM_CLASS);
+        customExecutor = loadCustomExecutor(className);
     }
 
-    @SuppressWarnings("rawtypes")
     public JTestMeResult executeTestMe() {
         final JTestMeResult result = super.getResult();
-
-        try {
-            JTestMeExecutor executor = null;
-            final Class clase = Class.forName(className);
-            final Constructor[] constructors = clase.getConstructors();
-            if (constructors != null && constructors.length > 0) {
-                for (final Constructor constructor : constructors) {
-                    final Class[] parametersClass = constructor.getParameterTypes();
-                    if (parametersClass == null || parametersClass.length == 0) {
-                        executor = (JTestMeExecutor) clase.newInstance();
-                        break;
-                    } else if (parametersClass.length == 1 && parametersClass[0].equals(Map.class)) {
-                        executor = (JTestMeExecutor) constructor.newInstance(getParams());
-                        break;
-                    } else if (parametersClass.length == 2 && parametersClass[0].equals(String.class)
-                            && parametersClass[1].equals(Map.class)) {
-                        executor = (JTestMeExecutor) constructor.newInstance(getName(), getParams());
-                        break;
-                    }
-                }
+        if (customExecutor == null) {
+            result.setMessage("No se ha cargado correctamente el custom executor: " + getName());
+        } else {
+            try {
+                final JTestMeResult resultCustomExecutor = customExecutor.executeTestMe();
+                result.setSuscess(resultCustomExecutor.isSuscess());
+                result.setMessage(resultCustomExecutor.getMessage());
+                result.setCause(resultCustomExecutor.getCause());
+            } catch (final Throwable e) {
+                result.setCause(e);
             }
-            if (executor != null) {
-                final JTestMeResult resultExecutor = executor.executeTestMe();
-                result.setSuscess(resultExecutor.isSuscess());
-                result.setMessage(resultExecutor.getMessage());
-            } else {
-                result.setMessage("No se ha podido cargar el custom executor: " + className);
-            }
-        } catch (final ClassNotFoundException e) {
-            result.setCause(e);
-        } catch (final InstantiationException e) {
-            result.setCause(e);
-        } catch (final Throwable e) {
-            result.setCause(e);
         }
         return result;
     }
 
+    private JTestMeExecutor loadCustomExecutor(final String className) {
+        JTestMeExecutor executor = null;
+        try {
+            executor = JTestMeUtils.loadExecutor(className, getName(), getParams());
+        } catch (final Throwable e) {
+            JTestMeLogger.warn("No se ha podido cargar el custom executor: " + className, e);
+        }
+        return executor;
+    }
 }
