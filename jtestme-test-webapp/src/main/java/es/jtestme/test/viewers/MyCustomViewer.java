4eccbef821bcb93c08f03705120e4c8ee3257ce7
carlos.alonso.gonzalez <carlos.alonso.gonzalez@gmail.com>
2013-03-24 22:18:51 +0000

    Formateado de código

--- jtestme-test-webapp/src/main/java/es/jtestme/test/viewers/MyCustomViewer.java
+++ jtestme-test-webapp/src/main/java/es/jtestme/test/viewers/MyCustomViewer.java
@@ -7,21 +7,22 @@ import es.jtestme.viewers.Viewer;
 
 public class MyCustomViewer implements Viewer {
 
-	public String getContentType() {
-		return "text/plain";
-	}
+    public String getContentType() {
+        return "text/plain";
+    }
 
-	public String getContentViewer(List<VerificatorResult> results) {
-		boolean error = false;
-		StringBuilder errorMessage = new StringBuilder();
-		errorMessage.append("ERROR").append(NEW_LINE);
-		for (VerificatorResult result : results) {
-			if (!result.isSuscess()) {
-				error = true;
-				errorMessage.append("\t - ").append(result.getName()).append(": ").append(result.getMessage()).append(NEW_LINE);
-			}
-		}
-		return !error ? "OK" : errorMessage.toString();
-	}
+    public String getContentViewer(final List<VerificatorResult> results) {
+        boolean error = false;
+        final StringBuilder errorMessage = new StringBuilder();
+        errorMessage.append("ERROR").append(NEW_LINE);
+        for (final VerificatorResult result : results) {
+            if (!result.isSuscess()) {
+                error = true;
+                errorMessage.append("\t - ").append(result.getName()).append(": ").append(result.getMessage())
+                        .append(NEW_LINE);
+            }
+        }
+        return !error ? "OK" : errorMessage.toString();
+    }
 
 }
