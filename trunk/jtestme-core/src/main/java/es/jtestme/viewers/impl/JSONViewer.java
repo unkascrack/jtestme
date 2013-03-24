8d4c031c9e5e72006891515e395ee0f9a13c4939
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@fd7ca0d8-11da-9f73-657e-4996fc3685d9>
2013-03-24 22:17:40 +0000

    Revisión viewer JSON

--- trunk/jtestme-core/src/main/java/es/jtestme/viewers/impl/JSONViewer.java
+++ trunk/jtestme-core/src/main/java/es/jtestme/viewers/impl/JSONViewer.java
@@ -14,15 +14,31 @@ public class JSONViewer extends AbstractViewer {
         final StringBuilder builder = new StringBuilder();
         builder.append(header());
         if (results != null && !results.isEmpty()) {
-            for (final VerificatorResult result : results) {
-                builder.append("\"servicio\" : {").append(NEW_LINE);
-                builder.append("\"nombre\" : \"").append(result.getName()).append("\"").append(NEW_LINE);
-                builder.append("\"estado\" : \"").append(result.getSuscessString()).append("\"").append(NEW_LINE);
+            for (int i = 0; i < results.size(); i++) {
+                final VerificatorResult result = results.get(i);
+                builder.append(TAB).append("\"verificator\" : {").append(NEW_LINE);
+                builder.append(TAB).append(TAB).append("\"type\" : \"").append(result.getType()).append("\"")
+                        .append(NEW_LINE);
+                builder.append(TAB).append(TAB).append("\"name\" : \"").append(result.getName()).append("\"")
+                        .append(NEW_LINE);
+                builder.append(TAB).append(TAB).append("\"description\" : \"").append(result.getDescription())
+                        .append("\"").append(NEW_LINE);
+                builder.append(TAB).append(TAB).append("\"success\" : \"").append(result.getSuscessString())
+                        .append("\"").append(NEW_LINE);
                 if (!result.isSuscess()) {
-                    builder.append("\"descError\" : \"").append(result.getMessage()).append("\"").append(NEW_LINE);
-                    builder.append("\"accionError\" : \"").append(result.getResolution()).append("\"").append(NEW_LINE);
+                    builder.append(TAB).append(TAB).append("\"message\" : \"").append(result.getMessage()).append("\"")
+                            .append(NEW_LINE);
+                    builder.append(TAB).append(TAB).append("\"cause\" : \"").append(result.getCauseString())
+                            .append("\"").append(NEW_LINE);
+                    builder.append(TAB).append(TAB).append("\"resolution\" : \"").append(result.getResolution())
+                            .append("\"").append(NEW_LINE);
                 }
-                builder.append("}").append(NEW_LINE);
+
+                if (i == results.size() - 1) {
+                    builder.append(TAB).append("}").append(NEW_LINE);
+                } else {
+                    builder.append(TAB).append("},").append(NEW_LINE);
+                }
             }
         }
         builder.append(footer());
@@ -31,7 +47,7 @@ public class JSONViewer extends AbstractViewer {
 
     private String header() {
         final StringBuilder builder = new StringBuilder();
-        builder.append("\"verificarSistema\" : {").append(NEW_LINE);
+        builder.append("\"verificators\" : {").append(NEW_LINE);
         return builder.toString();
     }
 
