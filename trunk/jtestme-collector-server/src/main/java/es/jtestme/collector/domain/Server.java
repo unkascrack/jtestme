71b7ba1115ff6ddc51506425411f3e45b7013d1b
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@fd7ca0d8-11da-9f73-657e-4996fc3685d9>
2013-04-07 19:57:23 +0000

    Revisado domain server

--- trunk/jtestme-collector-server/src/main/java/es/jtestme/collector/domain/Server.java
+++ trunk/jtestme-collector-server/src/main/java/es/jtestme/collector/domain/Server.java
@@ -1,21 +1,23 @@
 package es.jtestme.collector.domain;
 
-import es.jtestme.collector.domain.reference.ServerType;
 import java.util.HashSet;
 import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
 import org.springframework.roo.addon.equals.RooEquals;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.serializable.RooSerializable;
 import org.springframework.roo.addon.tostring.RooToString;
+
+import es.jtestme.collector.domain.reference.ServerType;
 
 @RooJavaBean
 @RooToString
@@ -24,36 +26,38 @@ import org.springframework.roo.addon.tostring.RooToString;
 @RooSerializable
 public class Server {
 
-    @NotNull
-    @Enumerated
-    private ServerType serverType;
+	@NotNull
+	@Enumerated
+	private ServerType serverType;
 
-    @NotNull
-    @Size(max = 100)
-    private String name;
+	@NotNull
+	@Size(max = 100)
+	private String name;
 
-    @Size(max = 255)
-    private String url;
+	@NotNull
+	@Size(max = 255)
+	private String url;
 
-    @Size(max = 50)
-    private String username;
+	@Size(max = 50)
+	private String username;
 
-    @Size(max = 50)
-    private String password;
+	@Size(max = 50)
+	private String password;
 
-    private String proxyHost;
+	private String proxyHost;
 
-    private String proxyPort;
+	private String proxyPort;
 
-    private String proxyUser;
+	private String proxyUser;
 
-    private String proxyPassword;
+	private String proxyPassword;
 
-    private String trustStorePassword;
+	private String trustStorePassword;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private Set<ServerState> states = new HashSet<ServerState>();
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Environment environment;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Environment environment;
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private final Set<ServerState> states = new HashSet<ServerState>();
 }
