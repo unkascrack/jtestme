521ffba17fd693992870ffc9327304fe2f3eb0a5
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@fd7ca0d8-11da-9f73-657e-4996fc3685d9>
2013-04-09 14:41:27 +0000

    Revisado la clase JTestmeLogger, renombrando el método "loggerEnabled" por "setLoggerEnabled" y revisado el método para obtener el nombre de la clase que ejecuta el logger (category).

--- trunk/jtestme-core/src/test/java/es/jtestme/logger/JTestMeLoggerTest.java
+++ trunk/jtestme-core/src/test/java/es/jtestme/logger/JTestMeLoggerTest.java
@@ -7,16 +7,38 @@ public class JTestMeLoggerTest {
 
     @BeforeClass
     public static void setUp() {
-        JTestMeLogger.loggerEnabled(true);
+        JTestMeLogger.setLoggerEnabled(true);
     }
 
     @Test
     public void testDebug() {
-        JTestMeLogger.debug("");
+        JTestMeLogger.debug("debug");
     }
 
     @Test
     public void testInfo() {
-        JTestMeLogger.info("");
+        JTestMeLogger.info("info");
+    }
+
+    @Test
+    public void testWarn() {
+        JTestMeLogger.warn("warn");
+    }
+
+    @Test
+    public void testError() {
+        JTestMeLogger.error("error");
+    }
+
+    @Test
+    public void testLoggerInnerClass() {
+        new MyInnerClass();
+    }
+
+    class MyInnerClass {
+
+        public MyInnerClass() {
+            JTestMeLogger.error("error inner class");
+        }
     }
 }
